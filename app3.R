7f3aee85548b861bf13cdf4eec4b6266c56589f4
Danyulll <danielkrasnovdk@hotmail.com>
2023-11-29 10:38:29 -0800

    Finished second tab

--- app3.R
+++ app3.R
@@ -6,6 +6,9 @@ library(shinyMatrix)
 library(semantic.dashboard)
 library(shinythemes)
 library(thematic)
+library(DT)
+library(GGally)
+library(ggplot2)
 set.seed(231354)
 
 
@@ -13,7 +16,8 @@ ui <- dashboardPage(
   dashboardHeader(title = "DATA 419 Project Title"),
   dashboardSidebar(sidebarMenu(
     menuItem(tabName = "simulation", "Simulation"),
-    menuItem(tabName = "application", "Application")
+    menuItem(tabName = "eda", "EDA"),
+    menuItem(tabName = "pca & nmf", "PCA & NMF")
   )),
   dashboardBody(tabItems(
     tabItem(
@@ -47,10 +51,6 @@ ui <- dashboardPage(
       # 
       #                  \\end{align} $$")),width = 8
       # ),
-      
-      
-      
-      
       
       box(
         matrixInput(
@@ -119,148 +119,25 @@ ui <- dashboardPage(
         ),width = 4
       )
     ),
-    tabItem(tabName = "application", fluidPage(h1("application")))
-  )),#theme = "darkly"
+    tabItem(tabName = "eda",     fluidRow(
+      box(
+        title = "Dataframe Box",
+        width = 7,
+        dataTableOutput("bc")
+      ),
+      box(
+        verbatimTextOutput("descrip"),width = 5
+      ),box(selectInput("selectedColumn", "Select Column", choices = colnames(bc)),
+            tableOutput("summaryOutput"),width=3),
+      box(plotOutput("pairs1")),
+      box(plotOutput("pairs2"))
+    )),tabItem(tabName = "pca & nmf")
+  ))
 )
    
-
-# ui <- dashboardPage(
-#   dashboardHeader(title = "My Dashboard"),
-#   dashboardSidebar(
-#     width = 2,  # Adjust the width of the sidebar
-#     numericInput("sld1x", "Change mu1x:", value = 1),
-#     numericInput("sld1y", "Change mu1y:", value = 1),
-    # numericInput("sld2x", "Change mu2x:", value = 0),
-    # numericInput("sld2y", "Change mu2y:", value = 0),
-    # numericInput("sld3x", "Change mu3x:", value = -1),
-    # numericInput("sld3y", "Change mu3y:", value = -1),
-    # matrixInput(
-    #   "matrix1",
-    #   label = "G1",
-    #   value = matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2, byrow = TRUE),
-    #   rows = list(names = FALSE),
-    #   cols = list(names = FALSE),
-    #   class = "numeric"
-    # ),
-    # matrixInput(
-    #   "matrix2",
-    #   label = "G2",
-    #   value = matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2, byrow = TRUE),
-    #   rows = list(names = FALSE),
-    #   cols = list(names = FALSE),
-    #   class = "numeric"
-    # ),
-    # matrixInput(
-    #   "matrix3",
-    #   label = "G3",
-    #   value = matrix(c(1, 0, 0, 1), nrow = 2, ncol = 2, byrow = TRUE),
-    #   rows = list(names = FALSE),
-    #   cols = list(names = FALSE),
-    #   class = "numeric"
-    # ),
-    # numericInput("lambda", "Change lambda:", value = 0),
-    # numericInput("gamma", "Change gamma:", value = 0),
-    # sliderInput("slider1", "Slider 1:", min = 0, max = 1, value = 0.5),
-    # sliderInput("slider2", "Slider 2:", min = 0, max = 1, value = 0.5),
-    # sliderInput("slider3", "Slider 3:", min = 0, max = 1, value = 0.5),
-    # actionButton("regenerate", "(Re)generate Plot")
-#   ),
-#   dashboardBody(
-#     width = 10,  # Adjust the width of the body
-#     fluidRow(
-#       box(
-#         plotlyOutput("plot", height = "600px", width = "100%")  # Use 100% width
-#       )
-#     )
-#   )
-# )
-
-
-
-# 
-# ui <- fluidPage(fluidRow(
-#   column(
-#     6,
-#     numericInput("sld1x", "Change mu1x:", value = 1),
-#     numericInput("sld1y", "Change mu1y:", value = 1),
-#     numericInput("sld2x", "Change mu2x:", value = 0),
-#     numericInput("sld2y", "Change mu2y:", value = 0),
-#     numericInput("sld3x", "Change mu3x:", value = -1),
-#     numericInput("sld3y",
-#                  "Change mu3y:",
-#                  value = -1),
-#     matrixInput(
-#       "matrix1",
-#       label = "G1",
-#       value = matrix(
-#         c(1, 0, 0, 1),
-#         nrow = 2,
-#         ncol = 2,
-#         byrow = TRUE
-#       ),
-#       rows = list(names = FALSE),
-#       cols = list(names = FALSE),
-#       class = "numeric"
-#     ),
-#     matrixInput(
-#       "matrix2",
-#       label = "G2",
-#       value = matrix(
-#         c(1, 0, 0, 1),
-#         nrow = 2,
-#         ncol = 2,
-#         byrow = TRUE
-#       ),
-#       rows = list(names = FALSE),
-#       cols = list(names = FALSE),
-#       class = "numeric"
-#     ),
-#     matrixInput(
-#       "matrix3",
-#       label = "G3",
-#       value = matrix(
-#         c(1, 0, 0, 1),
-#         nrow = 2,
-#         ncol = 2,
-#         byrow = TRUE
-#       ),
-#       rows = list(names = FALSE),
-#       cols = list(names = FALSE),
-#       class = "numeric"
-#     ),
-#     numericInput("lambda", "Change lambda:", value = 0),
-#     numericInput("gamma", "Change gamma:", value = 0),
-#     sliderInput(
-#       "slider1",
-#       "Slider 1:",
-#       min = 0,
-#       max = 1,
-#       value = 0.5
-#     ),
-#     sliderInput(
-#       "slider2",
-#       "Slider 2:",
-#       min = 0,
-#       max = 1,
-#       value = 0.5
-#     ),
-#     sliderInput(
-#       "slider3",
-#       "Slider 3:",
-#       min = 0,
-#       max = 1,
-#       value = 0.5
-#     ),
-#     actionButton("regenerate", "(Re)generate Plot")
-#   ),
-#   column(3,
-#          plotlyOutput("gaussian_plot", height = "600px", width = "800px"))
-# ))
-
-
 server <- function(input, output, session) {
   
-  
+  # Tab 1
   plot_data <- eventReactive(input$regenerate, {
     # Population Params
     # sigma1 <- matrix(c(1, 0, 0, 1), 2, 2, byrow = TRUE)
@@ -360,6 +237,61 @@ server <- function(input, output, session) {
   output$plot <- renderPlotly({
     plot_data()
   })
+  
+  # Tab 2
+  bc <- read.csv("C:\\Users\\danie\\Desktop\\2023 W1\\DATA 419C\\Project\\DATA_419_Project\\breast-cancer.csv") 
+  bc <- na.omit(bc)
+  bc <- bc <- bc[,2:12]
+  bc$diagnosis <- factor(bc$diagnosis)
+  output$bc <- renderDataTable(bc,
+                               options = list(
+                                 scrollX = TRUE,  # Enable horizontal scrolling
+                                 scrollY = "300px",paging = FALSE  # Enable vertical scrolling with a fixed height
+                               ),
+                               callback = JS("table.column(0).visible(false);"),
+                               )
+  
+  output$descrip <- renderText({   # Create a character vector with bullet points
+      "diagnosis: Target: M - Malignant B - Benign
+      
+      radius_mean: Radius of Lobes
+
+      texture_mean: Mean of Surface Texture
+      
+      perimeter_mean: Outer Perimeter of Lobes
+      
+      area_mean: Mean Area of Lobes
+
+      smoothness_mean: Mean of Smoothness Levels
+
+      compactness_mean: Mean of Compactness
+
+      concavity_mean: Mean of Concavity
+
+      concave points_mean: Mean of Cocave Points"
+  })
+  
+  output$pairs1 <- renderCachedPlot({
+    ggpairs(bc, columns = 1:5, aes(color = diagnosis, alpha = 0.5),
+                     upper = list(continuous = wrap("cor", size = 3)))
+  },
+  cacheKeyExpr = { list() }
+  )
+  
+  output$pairs2 <- renderCachedPlot({
+    ggpairs(bc, columns = 5:10, aes(color = diagnosis, alpha = 0.5),
+                      upper = list(continuous = wrap("cor", size = 3)))
+  },
+  cacheKeyExpr = { list() }
+  )
+  
+  output$summaryOutput <- renderTable({
+    col_selected <- input$selectedColumn
+    if (!is.null(col_selected)) {
+      as.data.frame(summary(bc[, col_selected, drop = FALSE]))[,3]
+    }
+  })
+  
   
   
 }
